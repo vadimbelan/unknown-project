f9b7bf22bb8bd821feaee0b76fe343c9acd1c10f
vadimbelan <92176898+vadimbelan@users.noreply.github.com>
2024-06-30 15:24:21 +0300

    Add files via upload

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -9,16 +9,35 @@ default_location = {
     'Долгота': 0.0
 }
 
+# Словарь переводов стран
+country_translations = {
+    'RU': 'Россия',
+}
+
+# Словарь переводов регионов
+region_translations = {
+    'Komi': 'Коми',
+}
+
+# Словарь переводов городов
+city_translations = {
+    'Yemva': 'Емва',
+}
+
 # Получаем информацию о местоположении
 def get_location_info(defaults):
     try:
         g = geocoder.ip('me')
 
         if g.ok:
+            country = g.country if defaults['Страна'] == '' else defaults['Страна']
+            region = g.state if defaults['Регион'] == '' else defaults['Регион']
+            city = g.city if defaults['Город'] == '' else defaults['Город']
+
             return {
-                'Страна': g.country if defaults['Страна'] == '' else defaults['Страна'],
-                'Регион': g.state if defaults['Регион'] == '' else defaults['Регион'],
-                'Город': g.city if defaults['Город'] == '' else defaults['Город'],
+                'Страна': country_translations.get(country, country),
+                'Регион': region_translations.get(region, region),
+                'Город': city_translations.get(city, city),
                 'Широта': g.latlng[0] if defaults['Широта'] == 0.0 else defaults['Широта'],
                 'Долгота': g.latlng[1] if defaults['Долгота'] == 0.0 else defaults['Долгота']
             }
