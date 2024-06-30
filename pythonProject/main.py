17168f78529dd67a938f0f543c9b9a4569330ab3
vadimbelan <92176898+vadimbelan@users.noreply.github.com>
2024-07-01 00:03:57 +0300

    Step 4. Вывод новостей по геопозиции

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
 import geocoder
 
-# Заглушки для местоположения
 default_location = {
     'Страна': '',
     'Регион': '',
@@ -9,20 +10,19 @@ default_location = {
     'Долгота': 0.0
 }
 
-# Словарь переводов стран
+# Словари для перевода стран, регионов и городов
 country_translations = {
     'RU': 'Россия',
 }
 
-# Словарь переводов регионов
 region_translations = {
     'Komi': 'Коми',
 }
 
-# Словарь переводов городов
 city_translations = {
     'Yemva': 'Емва',
 }
+
 
 # Получаем информацию о местоположении
 def get_location_info(defaults):
@@ -47,8 +47,39 @@ def get_location_info(defaults):
         defaults['Ошибка'] = str(e)
         return defaults
 
+
+# Функция для парсинга новостей
+def parse_news(source):
+    search_url = f'https://ria.ru/search/?query={source}'
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Найти все элементы, содержащие новости
+    news_items = soup.find_all('div', class_='list-item')
+
+    news_list = []
+    for item in news_items:
+        title = item.find('a', class_='list-item__title').get_text(strip=True)
+        link = item.find('a', class_='list-item__title')['href']
+        date = item.find('div', class_='list-item__date').get_text(strip=True)
+        news_list.append({'title': title, 'link': link, 'date': date})
+
+    return news_list
+
+
 # Сохраняем информацию о местоположении в переменную location_info
 location_info = get_location_info(default_location)
 
 # Выводим информацию о местоположении на экран
 print(location_info)
+
+# Фильтрация местоположения по конкретной области
+source = location_info.get('Регион', '')
+
+# Парсим новости
+if source:
+    news = parse_news(source)
+    for news_item in news:
+        print(f"Title: {news_item['title']}\nLink: {news_item['link']}\nDate: {news_item['date']}\n")
+else:
+    print("Не удалось определить местоположение")
